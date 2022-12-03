c195e29690e10581641e6b22b60217b52ab56500
WonjunS <seowj0710@gmail.com>
2022-12-03 15:06:45 +0900

    feat: Member Entity created

---
+++ shopping_mall/src/main/java/com/example/shopping_mall/domain/Member.java
@@ -0,0 +1,22 @@
+package com.example.shopping_mall.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String phone;
+
+    @Embedded
+    private Address address;
+}
