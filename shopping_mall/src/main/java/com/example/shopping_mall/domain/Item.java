728d760d53eb65a57697dc1ab10c51d96e97cbb9
WonjunS <seowj0710@gmail.com>
2022-12-10 20:57:31 +0900

    update: Item entity modified

--- shopping_mall/src/main/java/com/example/shopping_mall/domain/Item.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/domain/Item.java
@@ -1,13 +1,21 @@
 package com.example.shopping_mall.domain;
 
+import com.example.shopping_mall.constant.ItemSellStatus;
+import com.example.shopping_mall.dto.ItemFormDto;
 import com.example.shopping_mall.exception.OutOfStackException;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "item")
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
     @Id @GeneratedValue
@@ -18,17 +26,28 @@ public class Item {
     private String detail;
 
     private int price;
-    private int stockQuantity;
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;
+
+    public void updateItem(ItemFormDto itemFormDto) {
+        this.name = itemFormDto.getName();
+        this.detail = itemFormDto.getDetail();
+        this.price = itemFormDto.getPrice();
+        this.quantity = itemFormDto.getQuantity();
+        this.itemSellStatus = itemFormDto.getItemSellStatus();
+    }
 
     public void addStock(int quantity) {
-        this.stockQuantity += quantity;
+        this.quantity += quantity;
     }
 
     public void removeStock(int quantity) {
-        int restStock = this.stockQuantity - quantity;
+        int restStock = this.quantity - quantity;
         if(restStock < 0) {
             throw new OutOfStackException("상품의 재고가 부족합니다.");
         }
-        this.stockQuantity = restStock;
+        this.quantity = restStock;
     }
 }
