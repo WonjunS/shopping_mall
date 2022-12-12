206d8537179461c0aef5652aab6426fdcbc6a040
WonjunS <seowj0710@gmail.com>
2022-12-12 21:28:55 +0900

    feat: CartItemRepository implemented

---
+++ shopping_mall/src/main/java/com/example/shopping_mall/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.shopping_mall.repository;
+
+import com.example.shopping_mall.domain.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+}
