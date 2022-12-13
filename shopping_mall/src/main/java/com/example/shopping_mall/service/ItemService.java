076ad24d246529bc9fda88a9182b4e6a21016487
WonjunS <seowj0710@gmail.com>
2022-12-13 22:22:24 +0900

    update: find, save, update methods implemented

--- shopping_mall/src/main/java/com/example/shopping_mall/service/ItemService.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/service/ItemService.java
@@ -1,23 +1,43 @@
-//package com.example.shopping_mall.service;
-//
-//import com.example.shopping_mall.domain.Item;
-//import com.example.shopping_mall.dto.ItemFormDto;
-//import com.example.shopping_mall.repository.ItemRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//@Service
-//@Transactional
-//@RequiredArgsConstructor
-//public class ItemService {
-//
-//    private final ItemRepository itemRepository;
-//
-//    public List<Item> findItems() {
-//        return itemRepository.findAll();
-//    }
-//
-//}
+package com.example.shopping_mall.service;
+
+import com.example.shopping_mall.domain.Item;
+import com.example.shopping_mall.dto.ItemFormDto;
+import com.example.shopping_mall.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto) throws Exception{
+
+        // 상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        return item.getId();
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto) {
+        Item item = itemRepository.findById(itemFormDto.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        item.updateItem(itemFormDto);
+
+        return item.getId();
+    }
+
+}
