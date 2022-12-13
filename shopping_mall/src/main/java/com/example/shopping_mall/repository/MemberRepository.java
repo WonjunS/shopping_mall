12d5c5246fd372147504c065ac6527cb00e0dce2
WonjunS <seowj0710@gmail.com>
2022-12-13 22:20:08 +0900

    update: findByEmail query added

--- shopping_mall/src/main/java/com/example/shopping_mall/repository/MemberRepository.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/repository/MemberRepository.java
@@ -2,8 +2,11 @@ package com.example.shopping_mall.repository;
 
 import com.example.shopping_mall.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findByEmail(String email);
+
 }
