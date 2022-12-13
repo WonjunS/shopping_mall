6656a71e802f76b551b15d58cc7715de27273127
WonjunS <seowj0710@gmail.com>
2022-12-13 22:12:25 +0900

    update: 중복 회원 테스트 코드 추가

--- shopping_mall/src/test/java/com/example/shopping_mall/service/MemberServiceTest.java
+++ shopping_mall/src/test/java/com/example/shopping_mall/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,9 +18,40 @@ class MemberServiceTest {
     @Autowired
     MemberService memberService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     @DisplayName("회원가입")
     public void createMember() {
+        // given
+        JoinFormDto joinFormDto = new JoinFormDto();
+        joinFormDto.setName("홍길동");
+        joinFormDto.setEmail("abc1@gmail.com");
+        joinFormDto.setPassword("123456");
+        joinFormDto.setPhone("123-4567");
+        joinFormDto.setStreet("1 Street");
+        joinFormDto.setCity("Seoul");
+        joinFormDto.setZipcode("1234");
+
+        // when
+        Member member = Member.createMember(joinFormDto, passwordEncoder);
+        Member savedMember = memberService.save(member);
+
+        // then
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getPassword(), savedMember.getPassword());
+        assertEquals(member.getPhone(), savedMember.getPhone());
+        assertEquals(member.getStreet(), savedMember.getStreet());
+        assertEquals(member.getCity(), savedMember.getCity());
+        assertEquals(member.getZipcode(), savedMember.getZipcode());
+    }
+
+    @Test
+    @DisplayName("회원가입 중복 테스트")
+    public void createMemberDuplication() throws Exception {
+        // given
         JoinFormDto joinFormDto = new JoinFormDto();
         joinFormDto.setName("홍길동");
         joinFormDto.setEmail("abc@gmail.com");
@@ -29,9 +61,11 @@ class MemberServiceTest {
         joinFormDto.setCity("Seoul");
         joinFormDto.setZipcode("1234");
 
-        Member member = Member.createMember(joinFormDto);
-        Member savedMember = memberService.save(member);
+        // when
+        Member member = Member.createMember(joinFormDto, passwordEncoder);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> memberService.save(member));
 
-        assertEquals(member.getName(), savedMember.getName());
+        // then
+        assertEquals("이미 존재하는 회원입니다.", exception.getMessage());
     }
 }
