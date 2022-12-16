165a4fa8f959d1971b7d14f15d600a8ba04e35cb
WonjunS <seowj0710@gmail.com>
2022-12-16 23:13:54 +0900

    update: cartitem repository updated

--- shopping_mall/src/main/java/com/example/shopping_mall/repository/CartItemRepository.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/repository/CartItemRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 }
