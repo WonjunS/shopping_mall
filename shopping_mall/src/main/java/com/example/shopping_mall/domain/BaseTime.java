3c9794dce6df14de3729283ebb43acf1c7cff4ac
WonjunS <seowj0710@gmail.com>
2022-12-16 23:10:39 +0900

    feat: records created date and modified date

---
+++ shopping_mall/src/main/java/com/example/shopping_mall/domain/BaseTime.java
@@ -0,0 +1,24 @@
+package com.example.shopping_mall.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
