c0aaeb0f21f2be439845dc684497560d21a0fd23
WonjunS <seowj0710@gmail.com>
2022-12-18 21:37:20 +0900

    update: Queries added for methods

--- shopping_mall/src/main/java/com/example/shopping_mall/repository/OrderRepository.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/repository/OrderRepository.java
@@ -2,11 +2,11 @@ package com.example.shopping_mall.repository;
 
 import com.example.shopping_mall.domain.Order;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
@@ -17,4 +17,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
            "order by o.orderDate desc")
     List<Order> findOrders(@Param("email") String email, Pageable pageable);
 
+    @Query("select count(o) from Order o " +
+           "where o.member.email = :email")
+    Long countOrders(String email);
 }
