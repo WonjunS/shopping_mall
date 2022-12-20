987fefcd1ecacdd66db6d1d80ce963301919dd45
WonjunS <seowj0710@gmail.com>
2022-12-20 23:05:56 +0900

    update: of method converted to static

--- shopping_mall/src/main/java/com/example/shopping_mall/dto/ItemFormDto.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/dto/ItemFormDto.java
@@ -39,7 +39,7 @@ public class ItemFormDto {
         return modelMapper.map(this, Item.class);
     }
 
-    private ItemFormDto of(Item item) {
+    public static ItemFormDto of(Item item) {
         return modelMapper.map(item, ItemFormDto.class);
     }
 
