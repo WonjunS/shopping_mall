3c5ba284f62842e974b178800d9ab2eb85c3711b
WonjunS <seowj0710@gmail.com>
2022-12-23 00:13:48 +0900

    update: parameter name changed

--- shopping_mall/src/main/java/com/example/shopping_mall/dto/CartItemDto.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/dto/CartItemDto.java
@@ -9,10 +9,10 @@ import javax.validation.constraints.NotNull;
 @Getter @Setter
 public class CartItemDto {
 
-    @NotNull
-    private Long id;
+    @NotNull(message = "상품 아이디는 필수 입력 값입니다.")
+    private Long itemId;
 
-    @Min(value = 1)
+    @Min(value = 1, message = "상품은 최소 1개 이상 선택해야 합니다.")
     private int count;
 
 }
