9b5dc0d5851aeb9d33159f01be87f41a96e0809a
WonjunS <seowj0710@gmail.com>
2022-12-22 15:14:05 +0900

    update: main page load method modified

--- shopping_mall/src/main/java/com/example/shopping_mall/controller/MainController.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/controller/MainController.java
@@ -1,13 +1,19 @@
 package com.example.shopping_mall.controller;
 
 import com.example.shopping_mall.domain.Item;
+import com.example.shopping_mall.dto.ItemSearchDto;
+import com.example.shopping_mall.dto.MainItemDto;
 import com.example.shopping_mall.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,9 +22,14 @@ public class MainController {
     private final ItemService itemService;
 
     @GetMapping(value = "/")
-    public String main(Model model) {
-        List<Item> list = itemService.findItems();
-        model.addAttribute("list", list);
+    public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
+        Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+
         return "main";
     }
 }
