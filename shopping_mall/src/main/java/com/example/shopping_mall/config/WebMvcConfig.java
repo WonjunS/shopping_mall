5c54055506f09238f23a2408e8b45d6d1d01fc40
WonjunS <seowj0710@gmail.com>
2022-12-24 00:11:17 +0900

    update: add configuration annotation and change resource location

--- shopping_mall/src/main/java/com/example/shopping_mall/config/WebMvcConfig.java
+++ shopping_mall/src/main/java/com/example/shopping_mall/config/WebMvcConfig.java
@@ -1,17 +1,15 @@
 package com.example.shopping_mall.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Value("${uploadPath}")
-    String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations(uploadPath);
+                .addResourceLocations("file:///C:/Users/User/Desktop/item");
     }
 }
