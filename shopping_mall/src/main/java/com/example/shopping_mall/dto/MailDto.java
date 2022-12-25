b41264e25c135bdfde005c0fa6732a271b440a01
WonjunS <seowj0710@gmail.com>
2022-12-25 19:34:16 +0900

    feat: add mail dto class for email service

---
+++ shopping_mall/src/main/java/com/example/shopping_mall/dto/MailDto.java
@@ -0,0 +1,15 @@
+package com.example.shopping_mall.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MailDto {
+
+    private String address;
+    private String title;
+    private String message;
+}
